528942567e5f5cd830c97f9fef8edae049185178
Jisha-Jehran <jishajehran@gmail.com>
2024-03-13 19:15:33 +0530

    first commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# COUNTDOWN 
+  
+  This repository was created for submitting task of creating a timer with a countdown of 10 to wish Independence day using Callback Hell function. 
---
+++ script.js
@@ -0,0 +1,60 @@
+// Creating a countdown for Independence Day using Callback Hell:
+var countdown = document.querySelector("#countdown") 
+var c = 10;
+setTimeout( ()=> { 
+    console.log("cd", c) 
+    countdown.innerText = c ;
+    c--;  
+    setTimeout( ()=> { 
+        console.log("cd", c) 
+        countdown.innerText = c ;
+        c--;  
+        setTimeout( ()=> { 
+            console.log("cd", c) 
+            countdown.innerText = c ;
+            c--;  
+            setTimeout( ()=> { 
+                console.log("cd", c) 
+                countdown.innerText = c ;
+                c--;  
+                setTimeout( ()=> { 
+                    console.log("cd", c) 
+                    countdown.innerText = c ;
+                    c--;  
+                    setTimeout( ()=> { 
+                        console.log("cd", c) 
+                        countdown.innerText = c ;
+                        c--;  
+                        setTimeout( ()=> { 
+                            console.log("cd", c) 
+                            countdown.innerText = c ;
+                            c--;  
+                            setTimeout( ()=> { 
+                                console.log("cd", c) 
+                                countdown.innerText = c ;
+                                c--;  
+                                setTimeout( ()=> { 
+                                    console.log("cd", c) 
+                                    countdown.innerText = c ;
+                                    c--;  
+                                    setTimeout( ()=> { 
+                                        console.log("cd", c) 
+                                        countdown.innerText = c ;
+                                        c--;  
+                                        setTimeout( ()=> { 
+                                            console.log("indep")
+                                            countdown.innerText = "HAPPY INDEPENDENCE DAY";  
+                                            countdown.style.color = "gold"; 
+                                        }, 1000)
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}, 1000)
+
+
